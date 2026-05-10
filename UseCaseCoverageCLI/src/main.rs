1f2b9207bce9883e4edf2f4d47d84e22cf23f1cc
Pedro Gómez <pedrovicente.gomez@gmail.com>
2026-05-10 16:19:17 +0200

    Improve help options

--- UseCaseCoverageCLI/src/main.rs
+++ UseCaseCoverageCLI/src/main.rs
@@ -12,14 +12,14 @@ fn help_message() -> String {
             "\x1b[1;38;5;226mucc v{version}\x1b[0m\n\n",
             "\x1b[1mUse Case Coverage CLI\x1b[0m\n",
             "Generate use case coverage analysis reports and vlaidate .ucc specifications\n\n",
-            "By Pedro Gomez - https://github.com/sponsors/pedrovgs\n\n",
             "\x1b[1;38;5;208mUsage:\x1b[0m\n",
             "  \x1b[38;5;159mucc [OPTIONS] [COMMAND]\x1b[0m\n\n",
             "\x1b[1;38;5;208mOptions:\x1b[0m\n",
             "  \x1b[38;5;120m-h, --help\x1b[0m    Show this help message\n\n",
             "\x1b[1;38;5;208mCommands:\x1b[0m\n",
             "  \x1b[38;5;117mreport\x1b[0m        Generate an HTML report with features, use cases, and bugs analysis\n",
-            "  \x1b[38;5;117mlint\x1b[0m          Explore and validate .ucc files, ensuring the format is correct and nothing is missing\n"
+            "  \x1b[38;5;117mlint\x1b[0m          Explore and validate .ucc files, ensuring the format is correct and nothing is missing\n\n",
+            "By Pedro Gomez - https://github.com/sponsors/pedrovgs\n"
         ),
         version = env!("CARGO_PKG_VERSION")
     )
